7478073d0194c80c891782694cb731e2ba7d1fb4
theghostmac <theghostmac@gmail.com>
2023-08-29 14:32:58 +0100

    feature: refactored config files stuff

--- after/plugin/wakatime.lua
+++ after/plugin/wakatime.lua
@@ -1,5 +1,1 @@
--- vim.g.wakatime_api_key = os.getenv('WAKATIME_API_KEY') 
-
-local config = require('config')
-
-vim.g.wakatime_api_key = config.wakatime_api_key
+ vim.g.wakatime_api_key = os.getenv('WAKATIME_API_KEY')
